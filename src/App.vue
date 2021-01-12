aa70e02e0702646639904fa4e806bf27226f9529
touka.hanetsuki <touka.hanetuki@gmail.com>
2021-01-12 11:25:00 +0900

    add: InputTaskForm.vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,75 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Vue3 TODO APP</h1>
+
+  <h2>登録フォーム</h2>
+  <InputTaskForm @submit="addToTasks" />
+  <pre><code>{{ tasks }}</code></pre>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import InputTaskForm from '@/components/InputTaskForm.vue'
+import { v4 as uuid } from 'uuid';
+import dayjs from 'dayjs';
+
+/** タスクリストを保存するストレージのKEY */
+const STORAGE_KEY = 'tasks';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    InputTaskForm
+  },
+  data: () => ({
+    tasks: [] // タスクリスト
+  }),
+  created() {
+    // タスクリストの取得
+    this.fetchTasksToStorage();
+  },
+  methods: {
+    /** リストにタスクを追加する */
+    addToTasks(params) {
+      // 入力値を元にタスクを作成する
+      const task = this.createTask(params);
+
+      // リストにタスクを追加する
+      this.tasks.push(task);
+
+      // タスクリストの保存
+      this.saveTasksToStorage();
+    },
+    /** 引数を元にタスクを作成する */
+    createTask(params) {
+      return {
+        id: uuid(), // 固有ID
+        createdAt: dayjs().format('YYYY-MM-DDTHH:mm'), // 現在時刻
+        compuleteAt: null, // 完了時刻
+        status: 0, // タスクステータス
+        ...params // スプレッド演算子で引数を分解
+      };
+    },
+    /** ローカルストレージからタスクリストを取得する */
+    fetchTasksToStorage() {
+      // ローカルストレージからリストを取得
+      const tasks = localStorage.getItem(STORAGE_KEY);
+
+      // リストの存在確認
+      if (tasks) {
+        // 取得したリストをdataに上書き
+        this.tasks = JSON.parse(tasks);
+      }
+    },
+    /** ローカルストレージにタスクリストを保存する */
+    saveTasksToStorage() {
+      // ローカルストレージにタスクリストを保存
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(this.tasks));
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  h1 {
+    text-align: center;
+  }
 </style>
---
+++ src/components/InputTaskForm.vue
@@ -0,0 +1,70 @@
+<template>
+  <form @submit.prevent="submit">
+    <p>
+      <label>
+        タスク名
+        <input
+          v-model="form.taskName"
+          type="text"
+          required
+        >
+      </label>
+    </p>
+    <p>
+      <label>
+        期日
+        <input
+          v-model="form.deadlineAt"
+          type="datetime-local"
+          :min="minDeadlineAt"
+          required
+        >
+      </label>
+    </p>
+    <p>
+      <input
+        type="submit"
+        value="作成"
+      >
+    </p>
+  </form>
+</template>
+
+<script>
+import dayjs from 'dayjs';
+
+/** 現在の時刻を取得 */
+const getCurrentAt = () => dayjs().format('YYYY-MM-DDTHH:mm');
+
+/** 入力フォームの初期値を作成 */
+const initializeForm = () => ({
+  taskName: '',
+  deadlineAt: getCurrentAt()
+});
+
+export default {
+  name: "InputTaskForm",
+  emits: {
+    submit: null
+  },
+  data: () => ({
+    form: initializeForm(), // 入力フォーム
+  }),
+  computed: {
+    /** 締め切り日の最小値の算出 */
+    minDeadlineAt() {
+      return getCurrentAt();
+    }
+  },
+  methods: {
+    /** 親コンポーネントに入力値を送る */
+    submit() {
+      // 親コンポーネントに入力値を送る
+      this.$emit('submit', this.form);
+
+      // 入力値の初期化
+      this.form = initializeForm();
+    }
+  }
+}
+</script>
