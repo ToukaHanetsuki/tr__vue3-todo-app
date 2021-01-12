53bbba42a93570a52ad687540dac8de1aad755a1
touka.hanetsuki <touka.hanetuki@gmail.com>
2021-01-12 17:03:59 +0900

    fix: taskmodelの順序変更

--- src/model/taskModel.js
+++ src/model/taskModel.js
@@ -6,12 +6,11 @@ export class TaskModel {
     name,
     deadlineAt
   }) {
-    this.completeAt = undefined, // 完了時刻
-    this.createdAt = dayjs().format('YYYY-MM-DDTHH:mm'), // 現在時刻
     this.id = uuid(); // 固有ID
-    this.status = 0, // タスクステータス
-
     this.name = name; // タスク名
+    this.status = 0; // タスクステータス
     this.deadlineAt = deadlineAt; // 締め切り日
+    this.completeAt = undefined; // 完了時刻
+    this.createdAt = dayjs().format('YYYY-MM-DDTHH:mm'); // 現在時刻
   }
 }
